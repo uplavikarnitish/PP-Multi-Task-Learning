07f9348fe4f2c2d0355e112f7ebae18e92f2576b
Nitish M. Uplavikar <uplavikarnitish@users.noreply.github.com>
2021-01-15 20:50:49 -0600

    20180603 - backup
    Google Drive file name: ppmtl_bck_10.tar.gz

--- ppmtlutils.h
+++ ppmtlutils.h
@@ -4,23 +4,26 @@
 #define ENCR_DEB_LASSO_FRM_CL_FNAME "encr_db_l_cl.dat"
 #define ENCR_DEB_LASSO_NORM2 "encr_db_norm2.dat"
 #define ENCR_DEC_BITS_FNAME "encr_dec_bits.dat"
+#define SUPPORT_THRESHOLD_S1_FNAME "threshold_sq.dat"
+#define V_FNAME "v.dat"
+#define E_SUPP_FNAME "e_supp.dat"
 #define DELIMITER ","
 #define SCALEUP_BY 1000
+#define SUPPORT_DOM SCALEUP_BY*50
 #define S1_HOSTNAME "127.0.0.1"
 #define S2_HOSTNAME "127.0.0.1"
 #define C_TO_S1_PORT_NO 9081
 #define S1_TO_S2_PORT_NO 9082
 
-
-
-int append_file_name_to_directory( char *dir, int dir_sz, char *fname )
+typedef struct _sbd_sc_file_names_param
 {
-	if ( dir == NULL || fname == NULL )
-	{
-		fprintf(stderr, "%s:%d:: ERROR! BAD ARGS PASSED!\n", __func__, __LINE__);
-		return -1;
-	}
-	strncat(dir, "/", dir_sz - 1);
-	strncat(dir, fname, dir_sz - 1);
-	return 0;
-}
+	char *working_dir;
+	char *encr_norm2_file_name; //stores name of i/p file with list of E(norm2)
+	char *sup_threshold_file_name; //stores name of i/p file with list of $\Lambda^{2}$
+	char *op_encr_dec_bits_file_name; //stores name of o/p file in which norm2 bits would be decomposed and encrypted
+	char *v_file_name; //stores name of o/p file in which support threshold bits would be decomposed
+	char *e_supp_file_name; //stores name of o/p file in which encr.s of supports based on thresholds derived using sc_optimized() are stored
+
+
+}sbd_sc_file_names_param;
+
--- s1.c
+++ s1.c
@@ -347,7 +347,454 @@ clean_up:
 //	return err;
 //}
 
+int write_sup_thresholds_to_file(char *sup_threshold_file_name, long p, long domain)
+{
+	int err;
+	FILE *fp;
+	long val, i;
+	char *delimiter;
+
+	if ( (fp = fopen(sup_threshold_file_name, "w")) == NULL )
+	{
+		fprintf(stderr, "%s:%d:: ERROR! Cannot open file:%s\n", __func__, __LINE__, sup_threshold_file_name);
+		err = -1;
+		goto clean_up;
+	}
+	srand(time(NULL));
+	for ( i = 0; i < p; i++ )
+	{
+		long v = rand() % domain;//TODO: remove mod
+		v = (v * ((long)pow(-1, (v%2)))) % domain;
+		if ( v < 0 )
+		{
+			v = v * (-1);
+		}
+		if ( i != 0 )
+		{
+			delimiter = "\n";
+		}
+		else
+		{
+			delimiter = "";
+		}
+
+		if ( (err = fprintf(fp, "%s%ld", delimiter, v)) < 0 )
+		{
+			fprintf(stderr, "%s:%d:: fprintf() error! i:%d, err:%d\n", __func__, __LINE__, i, err);
+			goto clean_up;
+		}
+	}
+	err = 0;
+clean_up:
+	if ( fp )
+	{
+		fclose(fp);
+	}
+	return err;
+}
+
+
+int decompose_and_compare(long p, long l, sbd_sc_file_names_param *params, int s1_s2_socket, const char *context)
+{
+	int err = 0;
+	FILE *fp_n, *fp_s, *fp_e_s;
+	long num_read = 0;
+	mpz_t encr_norm2;
+	mpz_t e_s;
+	long sup_val;
+	char *delimiter;
+
+	char *working_dir = params->working_dir;
+	char *encr_norm2_file_name = params->encr_norm2_file_name; //stores name of i/p file with list of E(norm2)
+	char *sup_threshold_file_name = params->sup_threshold_file_name; //stores name of i/p file with list of $\Lambda^{2}$
+	char *op_encr_dec_bits_file_name = params->op_encr_dec_bits_file_name; //stores name of o/p file in which norm2 bits would be decomposed and encrypted
+	char *v_file_name = params->v_file_name; //stores name of o/p file in which support threshold bits would be decomposed
+	char *e_supp_file_name = params->e_supp_file_name; //stores name of o/p file in which encr.s of supports based on thresholds derived using sc_optimized() are stored
+
+
+	if ( (fp_n = fopen(encr_norm2_file_name, "r")) == NULL )
+	{
+		fprintf(stderr, "%s:%d:: ERROR! Cannot open file:%s\n", __func__, __LINE__, encr_norm2_file_name);
+		err = -1;
+		goto clean_up;
+	}
+
+	if ( (fp_s = fopen(sup_threshold_file_name, "r")) == NULL )
+	{
+		fprintf(stderr, "%s:%d:: ERROR! Cannot open file:%s\n", __func__, __LINE__, sup_threshold_file_name);
+		err = -2;
+		goto clean_up;
+	}
+
+	if ( (fp_e_s = fopen(e_supp_file_name, "w")) == NULL )
+	{
+		fprintf(stderr, "%s:%d:: ERROR! Cannot open file:%s\n", __func__, __LINE__, e_supp_file_name);
+		err = -3;
+		goto clean_up;
+	}
+
+	mpz_init(e_s);
+	mpz_init(encr_norm2);
+	num_read = 0;
+	while( 	(num_read < p) &&
+		((err = gmp_fscanf(fp_n, "%Zd,", encr_norm2))!=0)/*Something parsed*/ && (err != EOF) && 
+		((err = fscanf(fp_s, "%ld,", &sup_val))!=0)/*Something parsed*/ && (err != EOF) )
+	{
+		//first decompose
+		//to cut -start 1
+		if ((err = sbd(op_encr_dec_bits_file_name, encr_norm2, l, s1_s2_socket)) != 0)
+		{
+			fprintf(stderr, "%s:%d:: ERROR! returned:%d\n", __func__, __LINE__, err);
+			goto clean_up;
+		}
+		printf("[%s] %ld> SBD executed successfully!\n", context, (num_read+1));//dbg
+
+		//call the function to reverse the encr. bit order in file
+		if ( (err = reverse_file_line_by_line(op_encr_dec_bits_file_name, working_dir)) !=0 )
+		{
+			fprintf(stderr, "%s:%d:: ERROR! returned:%d\n", __func__, __LINE__, err);
+			goto clean_up;
+		}
+		printf("[%s] %ld> Reversed the dec. bit encr. successfully!\n", context, (num_read+1));//dbg
+		//to cut -stop 1
+
+		//to cut - 2 start
+		if ( (err = convert_to_bin_write_to_file(l, sup_val, v_file_name)) != 0 )
+		{
+			fprintf(stderr, "%s:%d:: ERROR! converting sup_val:%ld, err:%d\n", __func__, __LINE__, sup_val, err);
+			goto clean_up;
+		}
+		printf("[%s] %ld> sup_val's binary decomposition performed successfully, sup_val:%ld!\n", context, (num_read+1), sup_val);//dbg
+
+		//call the function to reverse the bit order in file to MSB first
+		if ( (err = reverse_file_line_by_line(v_file_name, working_dir)) !=0 )
+		{
+			fprintf(stderr, "%s:%d:: ERROR! returned:%d\n", __func__, __LINE__, err);
+			goto clean_up;
+		}
+		printf("[%s] %ld> Reversed the dec. bit successfully!\n", context, (num_read+1));//dbg
+		//to cut - 2 stop
+	
+		//now compare
+		//to cut - 3 start
+		//sc_optimized designed to be called by BOB only as per the protocol, i.e. anyone who calls is BOB in acting capacity
+		if ( (err = sc_optimized(e_s, op_encr_dec_bits_file_name, v_file_name, l, working_dir, s1_s2_socket, BOB))!=0 )
+		{
+			fprintf(stderr, "%s:%d:: ERROR! sc_optimized(), err:%d\n", __func__, __LINE__, err);
+			goto clean_up;
+		}
+		//debug - comparison result - start
+		//decrypt(e_s);dbg
+		//debug - comparison result - stop
+		//printf("\n[%s] %ld> Result: %ld > %ld?:%ld! \n", argv[0], val, v, mpz_get_si(e_s));//dbg
+		//debug - stop
+		printf("[%s] %ld> Computed comparison successfully!\n\n", context, (num_read+1));//dbg
+		//to cut - 3 stop
+
+
+		//Store the encrypted result of the comparison i.e. E(u>?v) in the file - start
+		if ( num_read != 0 )
+		{
+			//no delimiter after the last norm2 in the 
+			//file.
+			delimiter = "\n";
+		}
+		else
+		{
+			//first line/number, no delimiter prior
+			delimiter = "";
+		}
+		if( (err = gmp_fprintf(fp_e_s, "%s%Zd", delimiter, e_s)) < 0  )
+		{
+			fprintf(stderr, "%s:%d:: Cannot write to file:%s\n", __func__, __LINE__, e_supp_file_name);
+			goto clean_up;
+		}
+		//Store the encrypted result of the comparison i.e. E(u>?v) in the file - stop
+
+		num_read++;
+	}
+
+	
+	err = 0;
+clean_up:
+	if ( e_s )
+	{
+		mpz_clear(e_s);
+	}
+	if (encr_norm2)
+	{
+		mpz_clear(encr_norm2);
+	}
+	if ( fp_n )
+	{
+		fclose(fp_n);
+	}
+	if ( fp_s )
+	{
+		fclose(fp_s);
+	}
+	if ( fp_e_s )
+	{
+		fclose(fp_e_s);
+	}
+
+	return err;
+}
 int main(int argc, char const *argv[])
+{
+	int /*server_fd,*/ valread, err = 0;
+	char buffer[1024] = {0};
+	char *hello = "Hello from server";
+	int cl_s1_socket, s1_s2_socket ;
+	char cl_recv_file_name[1024];
+	char encr_norm2_file_name[1024];
+	char op_encr_dec_bits_file_name[1024];
+	char v_file_name[1024];
+	char sup_threshold_file_name[1024];
+	char e_supp_file_name[1024];
+	char *public_key_file_name = NULL;
+	char working_dir[1024];
+	long m, p;
+	long max_no_bits = 0;
+
+
+	if ( argc != 3 )
+	{
+		fprintf(stderr, "%s:%d:: ERROR! Usage:\n%s <public_key_file> <working_dir>\n", __func__, __LINE__, argv[0]);
+		return -1;
+	}
+
+	public_key_file_name = (char *)argv[1];
+	strncpy(g_key_file_name, public_key_file_name, sizeof(g_key_file_name));//Copy the public key file name - to be used later
+	strncpy(working_dir, argv[2], strlen(argv[2])>sizeof(working_dir)?
+	sizeof(working_dir):strlen(argv[2])+1);
+	memset(cl_recv_file_name, 0, sizeof(cl_recv_file_name));
+	strcpy(cl_recv_file_name, working_dir);
+
+	if ( (err = append_file_name_to_directory(cl_recv_file_name, sizeof(cl_recv_file_name), ENCR_DEB_LASSO_FRM_CL_FNAME)) != 0 )
+	{
+		fprintf(stderr, "%s:%d:: ERROR! appending err:%d\n", __func__, __LINE__, err);
+		goto clean_up;
+	}
+
+	if ( (cl_s1_socket = create_accept_socket_server(C_TO_S1_PORT_NO)) < 0 )
+	{
+		fprintf(stderr, "%s:%d:: ERROR! return:%d\n", __func__, cl_s1_socket);
+		return -1;
+	}
+	
+	if ( (err = recv_file(cl_s1_socket, cl_recv_file_name, (char *)argv[0])) !=0 )
+	{
+		fprintf(stderr, "%s:%d:: ERROR!!! Cannot receive file!\n", __func__, __LINE__);
+		return -2;
+	}
+	printf("[%s]: Received encr. deb. lasso sq. from client\n", argv[0]);
+	//valread = read( new_socket , buffer, 1024);
+	//printf("%s\n",buffer );
+	//send(new_socket , hello , strlen(hello) , 0 );
+	//printf("Hello message sent\n");
+
+	//Accept m
+	if ( (err = recv_long(cl_s1_socket, &m)) != 0 )
+	{
+		fprintf(stderr, "%s:%d:: [%s] ERROR! Cannot receive m, err:%d!\n", __func__, __LINE__, argv[0], err);
+		return -4;
+	}
+	printf("[%s] Received m:%lu from clients!\n", argv[0], m);
+
+	//Accept p
+	if ( (err = recv_long(cl_s1_socket, &p)) != 0 )
+	{
+		fprintf(stderr, "%s:%d:: [%s] ERROR! Cannot receive p, err:%d!\n", __func__, __LINE__, argv[0], err);
+		return -5;
+	}
+	printf("[%s] Received p:%lu from clients!\n", argv[0], p);
+
+	//Compute the encrypted norm 2 for each feature i, 0<=i<p
+	memset(encr_norm2_file_name, 0, sizeof(encr_norm2_file_name));
+	strcpy(encr_norm2_file_name, working_dir);
+
+	if ( (err = append_file_name_to_directory(encr_norm2_file_name, sizeof(encr_norm2_file_name), ENCR_DEB_LASSO_NORM2)) != 0 )
+	{
+		fprintf(stderr, "%s:%d:: ERROR! appending err:%d\n", __func__, __LINE__, err);
+		goto clean_up;
+	}
+
+	//we require secure operations from this stage onwards, initialize
+	//Paillier variables
+	init();
+	if ( (err = compute_encr_norm2(m, p, cl_recv_file_name, encr_norm2_file_name)) != 0 )
+	{
+		fprintf(stderr, "%s:%d:: ERROR! return:%d\n", __func__, __LINE__, err);
+		goto clean_up;
+	}
+	printf("[%s] Computed norm 2!\n", argv[0]);
+
+	//TODO: Remove auto calculation of max_no_bits from n
+	//if ( (err = get_n_size_in_bits(&max_no_bits)) != 0 )
+	//{
+	//	fprintf(stderr, "%s:%d:: ERROR! return:%d\n", __func__, __LINE__, err);
+	//	goto clean_up;
+	//}
+	max_no_bits = 20;//TODO: make this as a parameter
+
+	printf("[%s] Max. no. of bits: %ld!\n", argv[0], max_no_bits);
+
+	//Compute S1's thresholds - START
+	memset(sup_threshold_file_name, 0, sizeof(sup_threshold_file_name));
+	strcpy(sup_threshold_file_name, working_dir);
+	if ( (err = append_file_name_to_directory(sup_threshold_file_name, sizeof(sup_threshold_file_name), SUPPORT_THRESHOLD_S1_FNAME)) != 0 )
+	{
+		fprintf(stderr, "%s:%d:: ERROR! appending err:%d\n", __func__, __LINE__, err);
+		goto clean_up;
+	}
+	if ((err = write_sup_thresholds_to_file(sup_threshold_file_name, p, SUPPORT_DOM)) != 0)
+	{
+		fprintf(stderr, "%s:%d:: ERROR! writing thresholds, err:%d\n", __func__, __LINE__, err);
+		goto clean_up;
+	}
+	//Compute S1's thresholds - STOP
+
+	//Open connection to S2
+	if ((s1_s2_socket = create_connect_socket_client(S2_HOSTNAME, S1_TO_S2_PORT_NO )) < 0)
+	{
+		fprintf(stderr, "%s:%d:: ERROR! returned:%d", __func__, __LINE__, s1_s2_socket);
+		err = -3;
+		goto clean_up;
+	}
+	printf("[%s] Established the connection with server S2!\n", argv[0]);
+	
+	//printf("[%s] Calling append to store encr. dec. bits", argv[0]);//dbg
+	//Run the SBD on the program
+	memset(op_encr_dec_bits_file_name, 0, sizeof(op_encr_dec_bits_file_name));
+	//printf("[%s] Calling append to store encr. dec. bits", argv[0]);//dbg
+	strcpy(op_encr_dec_bits_file_name, working_dir);
+
+	//printf("[%s] Calling append to store encr. dec. bits", argv[0]);//dbg
+	if ( (err = append_file_name_to_directory(op_encr_dec_bits_file_name, sizeof(op_encr_dec_bits_file_name), ENCR_DEC_BITS_FNAME)) != 0 )
+	{
+		fprintf(stderr, "%s:%d:: ERROR! appending err:%d\n", __func__, __LINE__, err);
+		goto clean_up;
+	}
+	#if 0
+	//debug - start
+	mpz_t temp;
+	srand(time(NULL));
+	int val = ((int)rand()) % 128;
+	//mpz_init_set_ui(temp, 31);
+	encrypt(temp, val);
+	printf("[%s] Calling sbd for e_x:%d rev. bin.: ", argv[0], val);//dbg
+	#endif
+	#if 0
+	//to cut -start 1
+	if ((err = sbd(op_encr_dec_bits_file_name, /*op_encr_norm2_file_name*/temp, max_no_bits, s1_s2_socket)) != 0)
+	{
+		fprintf(stderr, "%s:%d:: ERROR! returned:%d\n", __func__, __LINE__, err);
+		goto clean_up;
+	}
+	printf("[%s] SBD executed successfully!\n", argv[0]);//dbg
+
+	//call the function to reverse the encr. bit order in file
+	if ( (err = reverse_file_line_by_line(op_encr_dec_bits_file_name, working_dir)) !=0 )
+	{
+		fprintf(stderr, "%s:%d:: ERROR! returned:%d\n", __func__, __LINE__, err);
+		goto clean_up;
+	}
+	printf("[%s] Reversed the dec. bit encr. successfully!\n", argv[0]);//dbg
+	//to cut -stop 1
+	#endif
+	#if 0
+	//Now generate v
+	long v = rand() % SUPPORT_DOM;//TODO: remove mod
+	v = (v * ((long)pow(-1, (v%2)))) % SUPPORT_DOM;
+	if ( v < 0 )
+	{
+		v = v * (-1);
+	}
+	printf("[%s] For sc_optimized(), v:%ld!\n", argv[0], v);//dbg
+	#endif
+	
+	//Decompose v in bits in the file, ordering is lsb first
+	memset(v_file_name, 0, sizeof(v_file_name));
+	strcpy(v_file_name, working_dir);
+	if ( (err = append_file_name_to_directory(v_file_name, sizeof(v_file_name), V_FNAME)) != 0 )
+	{
+		fprintf(stderr, "%s:%d:: ERROR! appending err:%d\n", __func__, __LINE__, err);
+		goto clean_up;
+	}
+
+	#if 0
+	//to cut - 2 start
+	if ( (err = convert_to_bin_write_to_file(max_no_bits, v, v_file_name)) != 0 )
+	{
+		fprintf(stderr, "%s:%d:: ERROR! converting v:%ld, err:%d\n", __func__, __LINE__, v, err);
+		goto clean_up;
+	}
+	printf("[%s] v's binary decomposition performed successfully, v:%ld!\n", argv[0], v);//dbg
+
+	//call the function to reverse the bit order in file to MSB first
+	if ( (err = reverse_file_line_by_line(v_file_name, working_dir)) !=0 )
+	{
+		fprintf(stderr, "%s:%d:: ERROR! returned:%d\n", __func__, __LINE__, err);
+		goto clean_up;
+	}
+	printf("[%s] Reversed the dec. bit successfully!\n", argv[0]);//dbg
+	//to cut - 2 stop
+	#endif
+	
+	//create file name to store the encrypted results of the secure comparison operation, E(u>?v)
+	memset(e_supp_file_name, 0, sizeof(e_supp_file_name));
+	strcpy(e_supp_file_name, working_dir);
+	if ( (err = append_file_name_to_directory(e_supp_file_name, sizeof(e_supp_file_name), E_SUPP_FNAME)) != 0 )
+	{
+		fprintf(stderr, "%s:%d:: ERROR! appending err:%d\n", __func__, __LINE__, err);
+		goto clean_up;
+	}
+
+	#if 0
+	//to cut - 3 start
+	mpz_t e_s;
+	mpz_init(e_s);
+	if ( (err = sc_optimized(e_s, op_encr_dec_bits_file_name, v_file_name, max_no_bits, working_dir, s1_s2_socket, BOB))!=0 )
+	{
+		fprintf(stderr, "%s:%d:: ERROR! sc_optimized(), err:%d\n", __func__, __LINE__, err);
+		goto clean_up;
+	}
+	//debug - comparison result - start
+	//decrypt(e_s);dbg
+	//debug - comparison result - stop
+	//printf("\n[%s] Result: %ld > %ld?:%ld! \n", argv[0], val, v, mpz_get_si(e_s));//dbg
+	//debug - stop
+	printf("[%s] Computed comparison successfully!\n", argv[0]);//dbg
+	//to cut - 3 stop
+	#endif
+	sbd_sc_file_names_param params;
+	params.working_dir = working_dir;
+	params.encr_norm2_file_name = encr_norm2_file_name;
+	params.sup_threshold_file_name = sup_threshold_file_name;
+	params.op_encr_dec_bits_file_name = op_encr_dec_bits_file_name;
+	params.v_file_name = v_file_name;
+	params.e_supp_file_name = e_supp_file_name;
+	if ( (err = decompose_and_compare(p, max_no_bits, &params, s1_s2_socket, argv[0])) != 0)
+	{
+		fprintf(stderr, "%s:%d:: ERROR! decompose_and_compare():%d\n", __func__, __LINE__, err);
+		goto clean_up;
+	}
+	printf("[./%s] All features decomposed and compared!\n", argv[0]);
+
+clean_up:
+	clear();
+	if ( cl_s1_socket )
+	{
+//		(shutdown(cl_s1_socket, SHUT_RDWR)==-1)?
+//			fprintf(stderr, "%s:%d:: Cannot close socket! errno:%d", errno):
+//			printf("[%s] Closed the connection with client!\n", argv[0]);
+	}
+
+	return 0;
+}
+#if 0
+int main_working(int argc, char const *argv[])
 {
 	int /*server_fd,*/ valread, err = 0;
 	char buffer[1024] = {0};
@@ -357,6 +804,7 @@ int main(int argc, char const *argv[])
 	char op_encr_norm2_file_name[1024];
 	char op_encr_dec_bits_file_name[1024];
 	char v_file_name[1024];
+	char sup_threshold_file_name[1024];
 	char *public_key_file_name = NULL;
 	char working_dir[1024];
 	long m, p;
@@ -443,7 +891,22 @@ int main(int argc, char const *argv[])
 	//}
 	max_no_bits = 20;//TODO: make this as a parameter
 
-	printf("[%s] Max. no. of bits (based on n): %ld!\n", argv[0], max_no_bits);
+	printf("[%s] Max. no. of bits: %ld!\n", argv[0], max_no_bits);
+
+	//Compute S1's thresholds - START
+	memset(sup_threshold_file_name, 0, sizeof(sup_threshold_file_name));
+	strcpy(sup_threshold_file_name, working_dir);
+	if ( (err = append_file_name_to_directory(sup_threshold_file_name, sizeof(sup_threshold_file_name), SUPPORT_THRESHOLD_S1_FNAME)) != 0 )
+	{
+		fprintf(stderr, "%s:%d:: ERROR! appending err:%d\n", __func__, __LINE__, err);
+		goto clean_up;
+	}
+	if ((err = write_sup_thresholds_to_file(sup_threshold_file_name, p, SUPPORT_DOM)) != 0)
+	{
+		fprintf(stderr, "%s:%d:: ERROR! writing thresholds, err:%d\n", __func__, __LINE__, err);
+		goto clean_up;
+	}
+	//Compute S1's thresholds - STOP
 
 	//Open connection to S2
 	if ((s1_s2_socket = create_connect_socket_client(S2_HOSTNAME, S1_TO_S2_PORT_NO )) < 0)
@@ -488,8 +951,8 @@ int main(int argc, char const *argv[])
 	}
 	printf("[%s] Reversed the dec. bit encr. successfully!\n", argv[0]);//dbg
 	//Now generate v
-	long v = rand() % 128;//TODO: remove mod
-	v = (v * ((long)pow(-1, v))) % 128;
+	long v = rand() % SUPPORT_DOM;//TODO: remove mod
+	v = (v * ((long)pow(-1, (v%2)))) % SUPPORT_DOM;
 	if ( v < 0 )
 	{
 		v = v * (-1);
@@ -527,7 +990,12 @@ int main(int argc, char const *argv[])
 		fprintf(stderr, "%s:%d:: ERROR! sc_optimized(), err:%d\n", __func__, __LINE__, err);
 		goto clean_up;
 	}
+	//debug - comparison result - start
+	//decrypt(e_s);dbg
+	//debug - comparison result - stop
+	//printf("\n[%s] Result: %ld > %ld?:%ld! \n", argv[0], val, v, mpz_get_si(e_s));//dbg
 	//debug - stop
+	printf("[%s] Computed comparison successfully!\n", argv[0]);//dbg
 
 clean_up:
 	clear();
@@ -548,3 +1016,4 @@ clean_up:
 
 	return 0;
 }
+#endif
